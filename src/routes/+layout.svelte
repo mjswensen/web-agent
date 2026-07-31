e9dd8bb1f9f2b899ec3af1e4071cdf7744a00984
Matt Swensen <matt@mjswensen.com>
2026-07-31 07:00:30 +0000

    Update icons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,6 +1,5 @@
 <script lang="ts">
 	import './layout.css';
-	import favicon from '$lib/assets/favicon.svg';
 	import { AppState } from '$lib/state/app-state.svelte';
 	import { setAppState } from '$lib/state/app-context.svelte';
 
@@ -8,5 +7,8 @@
 	setAppState(new AppState());
 </script>
 
-<svelte:head><link rel="icon" href={favicon} /></svelte:head>
+<svelte:head>
+	<link rel="icon" href="/web-agent.png" type="image/png" />
+	<link rel="apple-touch-icon" href="/web-agent.png" />
+</svelte:head>
 {@render children()}
